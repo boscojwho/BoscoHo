cf62b50e599d1c9be883ded9dd628044331a6301
Bosco Ho <boscojwho@gmail.com>
2024-06-04 04:23:49 -0700

    ~ Change text colour.

--- personal_web/styles.py
+++ personal_web/styles.py
@@ -2,7 +2,7 @@
 
 import reflex as rx
 
-text_color = rx.color("gray", 11)
+text_color = rx.color("white", 11)
 accent_text_color = rx.color("accent", 10)
 accent_color = rx.color("accent", 1)
 border_radius = "1em"
