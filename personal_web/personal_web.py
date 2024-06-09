133e5e981a1e142c346e080e955cd0be5af0f5d5
Bosco Ho <boscojwho@gmail.com>
2024-06-09 00:24:33 -0700

    + Documentation.

--- personal_web/personal_web.py
+++ personal_web/personal_web.py
@@ -44,6 +44,7 @@ for post in dir_list:
     # See reflex-web/pcweb/pages/blog/blog.py
     # This is how we can pass props into a rx.component.
     template.page(route=f"/coding/blog/{post}", title="Blog Post", show_sidebar_right=False)(
+        # Must pass `() -> rx.Component`, hence lambda function here.
         lambda: blog_post.blog_post(post)
     )
     # app.add_page(blog_post.blog_post, route=f"/coding/blog/{post}")
