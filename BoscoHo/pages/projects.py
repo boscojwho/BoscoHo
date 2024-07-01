501480610625eddb950180a4c99bd2dbd37e66da
Bosco Ho <boscojwho@gmail.com>
2024-07-01 01:44:51 -0700

    ~ *WIP* Update projects page.

--- BoscoHo/pages/projects.py
+++ BoscoHo/pages/projects.py
@@ -50,8 +50,49 @@ def projects() -> rx.Component:
             )
         ),
         rx.divider(),
+        rx.chakra.responsive_grid(
+            rx.link(
+                rx.vstack(
+                    rx.chakra.image(src="/fencathon_2_logo.png", width="44px", height="auto", borderRadius="9.8428"),
+                    # rx.text('Fencathon', weight="bold"),
+                    align="center"
+                ),
+                href="/"
+            ),
+            rx.link(
+                rx.vstack(
+                    rx.chakra.image(src="/fencathon_1_logo.png", width="44px", height="auto", borderRadius="9.8428"),
+                    # rx.text('Fencathon', weight="bold"),
+                    align="center"
+                ),
+                href="/"
+            ),
+            rx.link(
+                rx.vstack(
+                    rx.chakra.image(src="/atp_logo.png", width="44px", height="auto", borderRadius="9.8428"),
+                    # rx.text('Fencathon', weight="bold"),
+                    align="center"
+                ),
+                href="/"
+            ),
+            columns=[2, 3, 4],
+            spacing="4",
+        ),
         rx.text("Fencing"),
-        rx.text("RunGo"),
+        rx.divider(),
+        rx.chakra.responsive_grid(
+            rx.link(
+                rx.vstack(
+                    rx.chakra.image(src="/rungo_icon_old.png", width="44px", height="auto", borderRadius="9.8428"),
+                    rx.chakra.image(src="/rungo_icon_new.png", width="44px", height="auto"),
+                    # rx.text('Fencathon', weight="bold"),
+                    align="center"
+                ),
+                href="/"
+            ),
+            columns=[2, 3, 4],
+            spacing="4",
+        ),
     )
     # return rx.vstack(
     #     rx.chakra.card(
