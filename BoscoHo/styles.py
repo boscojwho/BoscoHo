77c54f00291d5a52a2dc3ab6850eb7069153d907
boscojwho <boscojwho@gmail.com>
2024-07-09 23:36:10 +0000

    Build: (869641e) Merge branch 'main' into github-pages
    * main:
      ~ Update deploy action

--- BoscoHo/styles.py
+++
@@ -1,9 +0,0 @@
-"""Styles for the app."""
-
-import reflex as rx
-
-text_color = rx.color("white", 11)
-accent_text_color = rx.color("accent", 10)
-accent_color = rx.color("accent", 1)
-border_radius = "1em"
-FONT_FAMILY = "Gluten"
--- BoscoHo/utils/md_metadata.py
+++
@@ -1,9 +0,0 @@
-import markdown
-from typing import Dict
-def md_metadata(md: str) -> Dict:
-    """
-    Returns Markdown metadata defined at top of .md file as a dictionary.
-    """
-    md_meta = markdown.Markdown(extensions=['meta'])
-    md_meta.convert(md)
-    return md_meta.Meta
